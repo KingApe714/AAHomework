865c2783e4e0086481bdd901afe312f3fc0f75f6
jonathandiaz <jonathan.diaz714@gmail.com>
2021-01-03 16:20:16 -0500

    finished assignment

--- W5D5_HW/movie_novice/queries.rb
+++ W5D5_HW/movie_novice/queries.rb
@@ -61,14 +61,20 @@ def alphabetized_actors
   # Note: Ubuntu users may find that special characters
   # are alphabetized differently than the specs.
   # This spec might fail for Ubuntu users. It's ok!
-
+  Actor
+    .select(:id, :name)
+    .order(:name)
+    .limit(10)
 end
 
 def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-
+  Actor
+    .select(:id, :name)
+    .joins(:movies)
+    .where('title = \'Pulp Fiction\'')
 end
 
 def uma_movies
@@ -76,5 +82,9 @@ def uma_movies
   # display the id, title, and year of movies Uma Thurman has acted in
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
-
+  Movie
+    .select(:id, :title, :yr)
+    .joins(:actors)
+    .where('name = \'Uma Thurman\'')
+    .order('yr ASC')
 end
