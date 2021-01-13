c76a217cc832e7cd6e2a21eafe146a33741afad1
jonathandiaz <jonathan.diaz714@gmail.com>
2021-01-13 12:36:43 -0500

    finished W8D3 HW

--- W8D3/jsPractice.js
+++ W8D3/jsPractice.js
@@ -67,8 +67,65 @@ function isSubstring(sen, sub) {
   return false
 }
 
-madLib('make', 'best', 'guac')
+function fizzBuzz(array) {
+  var arr = [];
+  for (var i = 0; i < array.length; i++) {
+    var num = array[i];
+    if ((num % 3 || num % 5) && !(num % 3 && num % 5)) {
+      arr.push(num)
+    }
+  }
+  return arr
+}
 
-console.log(isSubstring("time to program", "time"))
+function isPrime(num) {
+  if (num < 2 || num % 1 != 0 || (num % 2 === 0 && num != 2)) {
+    return false
+  }
+  for (var i = 3; i <= Math.floor(Math.sqrt(num)); i+=2) {
+    if (num % i === 0){
+      return false;
+    }
+  }
+  return true;
+}
 
-console.log(isSubstring("Jump for joy", "joys"))
+function nextPrime(num) {
+  let i = num + 1;
+  while (true) {
+    if (isPrime(i)) {
+      return i;
+    }
+    i++;
+  }
+}
+function firstNPrimes(n){
+  var arr = [2];
+  var count = 1;
+  while (count < n){
+    arr.push(nextPrime(arr[arr.length - 1]))
+    count++;
+  }
+  return arr
+}
+
+function sumOfNPrimes(n) {
+  let arr = firstNPrimes(n);
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+// madLib('make', 'best', 'guac')
+
+// console.log(isSubstring("time to program", "time"))
+
+// console.log(isSubstring("Jump for joy", "joys"))
+
+// console.log(fizzBuzz([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))
+
+// console.log(isPrime(1))
+
+console.log(sumOfNPrimes(4))
