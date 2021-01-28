fcb4b789cda9f9a279b807277e44ef957fb8c9d7
jonathandiaz <jonathan.diaz714@gmail.com>
2021-01-28 11:50:53 -0500

    created the store in Job Listing

--- W8D4/jsProblems2.js
+++ W8D4/jsProblems2.js
@@ -56,3 +56,18 @@ console.log(Jona);
 Jona.play();
 
 Jona.play();
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+Elephant.prototype.paradeHelper = function(elephant) {
+    console.log(`${elephant.name} is trotting by`)
+};
+
+herd.forEach( ele => {
+    ele.paradeHelper(ele)
+})
--- js_practice/practice.js
+++ js_practice/practice.js
@@ -81,3 +81,113 @@ garfield.play();
 const halfMyAge = myAge => myAge / 2;
 
 console.log(halfMyAge(30) === 15);
+
+function sayHelloNTimes(name, n) {
+
+    //greet is a closure. it captures variables for us (like name).
+    function greet() {
+        console.log( `Hi, ${name}!`)
+    }
+
+    for (let i = 0; i < n; i++) {
+        greet()
+    }
+}
+
+sayHelloNTimes("Bob", 3);
+sayHelloNTimes("Sally", 6)
+
+//free variables can be modified by closures. Consider the following
+function sum(nums) {
+    //count is a free variable in this example that function addNum closed around
+    let count = 0;
+
+    function addNum(num) {
+        count += num;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        addNum(nums[i]);
+    }
+
+    return count;
+}
+
+console.log(sum([1,3,5]));
+
+//we can use closures to pass down arguments to helper functions without explicitly listing them as arguments:
+
+function isPalindrome(string) {
+
+    function reverse() {
+        return string.split('').reverse().join('')
+    }
+
+    return string === reverse();
+}
+
+//Another major use of closures is to create private states:
+
+function createCounter() {
+    let count = 0;
+    // here ++count means increment the value of the count variable by 1 and then the count variable is evaluated in the statement.
+    // count++ means the count variable is evaluated in the statement and then the value is incremented by 1. If we used count++ our counter would be off by 1.
+    return () => ++count;
+}
+
+function Counter () {
+    this._count = 0;
+}
+  
+Counter.prototype.fire = function () {
+    this._count += 1;
+    return this._count;
+}
+  
+let counter = new Counter();
+console.log(counter.fire()); // 1
+counter.fire(); // 2
+console.log(counter._count) // 2
+counter._count = 0 // 0 (this works);
+
+console.log(counter._count)
+
+
+//this is a constructor function, need to initialize capitalized camelcased
+function Kitten(name, age) {
+    this.name = name;
+    this.age = age;
+
+    //we can eliminate this redundancy by using the constructor's prototype
+    // this.meow = function () {
+    //     console.log(this.name + ' says "MEOW!"');
+    // };
+}
+
+//constructor's prototype:
+Kitten.prototype.meow = function () {
+    console.log(this.name + ' says "meow!"');
+};
+
+let kitten = new Kitten("Earl", 2);
+
+k1 = new Kitten("Earl", 2);
+k2 = new Kitten("Houdini", 1);
+
+console.log(kitten)
+
+console.log(k1)
+console.log(k2)
+
+console.log(Object.getPrototypeOf(k1));
+console.log(Object.getPrototypeOf(k2));
+
+Kitten.caboodle = [k1, k2, new Kitten('Flying Merkel', 3)];
+
+Kitten.parade = function() {
+  Kitten.caboodle.forEach( kitten => {
+    kitten.meow();
+  });
+};
+
+Kitten.parade();
